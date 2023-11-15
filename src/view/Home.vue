88d4e8b01cc9a2b451f18265e40a69725eaff722
angler <2926686459@qq.com>
2023-11-15 21:21:14 +0800

    text

---
+++ src/view/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>HOME</div>
+</template>
+
+<script setup ></script>
+
+// views/Demo.vue
+<template>
+  <div>DEMO</div>
+</template>
+
+<script setup ></script>
+
+// App.vue
+<script setup ></script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+  #app {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
+
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from "path";
+import eslintPlugin from "vite-plugin-eslint"; //导入包
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    //设置别名
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  // 配置ESLint插件
+  plugins: [vue(), ],
+});
