59e4816dcc657d375badd68492488450057e464b
薛江槟 <1753456830@qq.com>
2024-02-28 11:58:53 +0800

    data界面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <Home></Home>
-  <News></News>
+  <!-- <login></login> -->
+  <!-- <Home></Home> -->
+  <Temp></Temp>
   <!-- <Blog></Blog> -->
   <!-- <Push></Push> -->
 </template>
@@ -9,12 +10,16 @@
 import Home from "./view/Home.vue";
 import News from "./view/News.vue";
 import Blog from "./view/Blog.vue";
+import Login from './view/login.vue';
+import Temp from '../src/components/Home-Data/data.vue'
 
 export default {
   components: {
     Home,
     News,
     Blog,
+    Login,
+    Temp
   },
 };
 </script>
