65efbce6780ad1a4b3e2c89b712b66f5597d2201
薛江槟 <1753456830@qq.com>
2024-02-28 14:05:53 +0800

    new fix

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <!-- <login></login> -->
+ 
   <Home></Home>
-  <!-- <Temp></Temp> -->
-  <news></news>
-  <!-- <Blog></Blog> -->
-  <!-- <Push></Push> -->
+  <!-- <News></News>
+  <Blog></Blog>
+  <Push></Push> -->
 </template>
 
 <script>
 import Home from "./view/Home.vue";
 import News from "./view/News.vue";
 import Blog from "./view/Blog.vue";
-import Login from './view/login.vue';
-import Temp from '../src/components/Home_routes(xjb)/routes.vue'
+
 
 export default {
   components: {
     Home,
     News,
     Blog,
-    Login,
-    Temp
+   
   },
 };
 </script>
