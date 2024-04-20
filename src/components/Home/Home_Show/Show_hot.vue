35bceec68f0f75f8aafa818fea05f95f8544bc37
angler <2926686459@qq.com>
2024-04-20 17:44:50 +0800

    首页 组件 进行重构 删除无用代码

--- src/components/Home/Home_Show/Show_hot.vue
+++ src/components/Home/Home_Show/Show_hot.vue
@@ -8,67 +8,68 @@
       <div class="title">—————— 热门推荐 ——————</div>
       <div class="shower">
         <showbox
-          class="first"
-          img_src="https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_1.png"
-          text="角子崖"
+          v-for="(item, index) in hotRecommendations"
+          :key="index"
+          :class="{ first: index === 0 }"
+          :img_src="item.imgSrc"
+          :text="item.text"
           path="/home_routes_news"
-          :query="{ store: 'recommend', page: 'jiaoziya' }"
-        ></showbox>
-        <showbox
-          img_src="https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_2.png"
-          text="吕梁山"
-          path="/home_routes_news"
-          :query="{ store: 'recommend', page: 'lvliangshan' }"
-        ></showbox>
-        <showbox
-          img_src="https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_3.png"
-          text="古交红豆山庄"
-          path="/home_routes_news"
-          :query="{ store: 'recommend', page: 'hongdou' }"
-        ></showbox>
-        <showbox
-          img_src="https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_4.png"
-          text="古交千佛寺"
-          path="/home_routes_news"
-          :query="{ store: 'recommend', page: 'qianfosi' }"
-        ></showbox>
-        <showbox
-          img_src="https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_5.png"
-          text="晋绥八分区旧址"
-          path="/home_routes_news"
-          :query="{ store: 'recommend', page: 'bafenqu' }"
-        ></showbox>
-        <showbox
-          img_src="https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_6.png"
-          text="古交水泉寨"
-          path="/home_routes_news"
-          :query="{ store: 'recommend', page: 'shuiquanzhai' }"
-        ></showbox>
-        <showbox
-          img_src="https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_7.png"
-          text="歇马武氏宅院"
-          path="/home_routes_news"
-          :query="{ store: 'recommend', page: 'xiema' }"
-        ></showbox>
-        <showbox
-          img_src="https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_8.png"
-          text="石千峰文化遗址"
-          path="/home_routes_news"
-          :query="{ store: 'recommend', page: 'shiqianfeng' }"
-        ></showbox>
-        <showbox
-          img_src="https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_9.png"
-          text="真能和尚灵塔"
-          path="/home_routes_news"
-          :query="{ store: 'recommend', page: 'zhenneng' }"
+          :query="item.query"
         ></showbox>
       </div>
     </div>
   </div>
 </template>
-
 <script setup>
 import showbox from "@/components/showbox.vue";
+import { ref } from "vue";
+const hotRecommendations = ref([
+  {
+    imgSrc: "https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_1.png",
+    text: "角子崖",
+    query: { store: "recommend", page: "jiaoziya" },
+  },
+  {
+    imgSrc: "https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_2.png",
+    text: "吕梁山",
+    query: { store: "recommend", page: "lvliangshan" },
+  },
+  {
+    imgSrc: "https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_3.png",
+    text: "古交红豆山庄",
+    query: { store: "recommend", page: "hongdou" },
+  },
+  {
+    imgSrc: "https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_4.png",
+    text: "古交千佛寺",
+    query: { store: "recommend", page: "qianfosi" },
+  },
+  {
+    imgSrc: "https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_5.png",
+    text: "晋绥八分区旧址",
+    query: { store: "recommend", page: "bafenqu" },
+  },
+  {
+    imgSrc: "https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_6.png",
+    text: "古交水泉寨",
+    query: { store: "recommend", page: "shuiquanzhai" },
+  },
+  {
+    imgSrc: "https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_7.png",
+    text: "歇马武氏宅院",
+    query: { store: "recommend", page: "xiema" },
+  },
+  {
+    imgSrc: "https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_8.png",
+    text: "石千峰文化遗址",
+    query: { store: "recommend", page: "shiqianfeng" },
+  },
+  {
+    imgSrc: "https://yunding-ljt.oss-cn-beijing.aliyuncs.com/hot_9.png",
+    text: "真能和尚灵塔",
+    query: { store: "recommend", page: "zhenneng" },
+  },
+]);
 </script>
 
 <style scoped>
@@ -89,13 +90,11 @@ import showbox from "@/components/showbox.vue";
   min-height: 400px;
   min-width: 550px;
   max-width: 1000%;
-  /* background-color: greenyellow; */
   margin-left: 0;
   margin-right: 0;
 }
 .title {
   position: relative;
-  /* background-color: grey; */
   height: 50px;
   top: -10%;
   width: 40%;
@@ -107,30 +106,23 @@ import showbox from "@/components/showbox.vue";
 }
 .shower {
   display: flex;
-  flex-direction: row;
-  /* justify-content: space-around; */
   justify-content: space-between;
   align-items: normal;
   flex-wrap: wrap;
   position: relative;
-  top: 0%;
+  top: 0;
   width: 100%;
   height: 80%;
-  /* left: 18%; */
-  /* background-color: indigo; */
 }
-
 .box {
   flex-basis: 18%;
   flex-grow: 1;
   height: 44%;
 }
 .box:hover {
-  /* animation: fly 0.75s; */
   animation-fill-mode: forwards;
   transform: translateY(-20px);
 }
-
 .first {
   flex-grow: 2;
   flex-basis: 38%;
