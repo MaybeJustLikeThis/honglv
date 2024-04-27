a4a743497a4b65fabf067a5f0427edd64fd26974
angler <2926686459@qq.com>
2024-04-27 18:59:12 +0800

    请求缓存的实现 和 搜索页完善

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,30 +1,84 @@
 import request from "./request";
+import cache from "./cache";
+
+//封装缓存函数 有一定缺陷 这个hashmap的键值对可能会重复 导致 当请求参数不完全相同时，请求可能需要完全相同的参数才能返回相同的响应。
+//然后就是缓存过期的和缓存大小的问时间题
+function createCacheableRequest(config) {
+  const { url, method, data, query } = config;
+  const cacheKey = JSON.stringify({ url, method, data, query });
+
+  if (cache[cacheKey]) {
+    return Promise.resolve(cache[cacheKey]);
+  }
+
+  return request(config).then((response) => {
+    cache[cacheKey] = response;
+    return response;
+  });
+}
 
 // e.g
 function getLogin() {
-  return request({
+  return createCacheableRequest({
     url: "/login",
     method: "get",
   });
 }
+
 function postLogin(params) {
-  return request({
+  return createCacheableRequest({
     url: "/login",
     method: "post",
     data: params,
   });
 }
+
 function getHomeTOPRoute() {
-  return request({
+  return createCacheableRequest({
     url: "/rounte/getTopRounte",
     method: "get",
   });
 }
+
 function getRouteInfo(args) {
-  return request({
+  return createCacheableRequest({
     url: "/rounte/getHomeRounte",
     method: "get",
     query: args,
   });
 }
-export { getLogin, postLogin, getHomeTOPRoute, getRouteInfo };
+
+function getTOPInformation() {
+  return createCacheableRequest({
+    url: "/news/getTopNews",
+    method: "get",
+  });
+}
+
+function getlike(id, like_type) {
+  return request({
+    url: "/like",
+    method: "get",
+    query: {
+      id: id,
+      status: like_type,
+    },
+  });
+}
+
+function getTopdestinations() {
+  return createCacheableRequest({
+    url: "/attraction/getTopAttraction",
+    method: "get",
+  });
+}
+
+export {
+  getLogin,
+  postLogin,
+  getHomeTOPRoute,
+  getRouteInfo,
+  getTOPInformation,
+  getlike,
+  getTopdestinations,
+};
