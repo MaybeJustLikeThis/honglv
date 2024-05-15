6ee230c30517d9aae72ce23c4231fac70119b186
angler <2926686459@qq.com>
2024-05-15 21:49:29 +0800

    首页搜索-级联选择

---
+++ src/components/Search/check_content.vue
@@ -0,0 +1,10 @@
+<template>
+ 
+</template>
+
+<script setup>
+import { ref, defineProps } from "vue";
+
+</script>
+
+<style scoped></style>
--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -3,18 +3,23 @@ import cache from "./cache";
 
 //封装缓存函数 有一定缺陷 这个hashmap的键值对可能会重复 导致 当请求参数不完全相同时，请求可能需要完全相同的参数才能返回相同的响应。
 //然后就是缓存过期的和缓存大小的问时间题
-function createCacheableRequest(config) {
+function createCacheableRequest(config, ttl = 600000) {
   const { url, method, data, query } = config;
   const cacheKey = JSON.stringify({ url, method, data, query });
 
-  if (cache[cacheKey]) {
-    return Promise.resolve(cache[cacheKey]);
+  const cachedItem = cache[cacheKey];
+  if (cachedItem && Date.now() - cachedItem.timestamp < ttl) {
+    return Promise.resolve(cachedItem.response);
   }
 
-  return request(config).then((response) => {
-    cache[cacheKey] = response;
-    return response;
-  });
+  return request(config)
+    .then((response) => {
+      cache[cacheKey] = { response, timestamp: Date.now() };
+      return response;
+    })
+    .catch((err) => {
+      console.error("Request failed", err);
+    });
 }
 
 // e.g
